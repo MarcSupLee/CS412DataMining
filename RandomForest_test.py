771e10306f8dcd1d3afdc2c9cdb0bf79025fa7bf
Minsup Lee <Minsup@wirelessprv-10-193-212-183.near.illinois.edu>
2018-04-19 20:59:20 -0500

    last TT

--- RandomForest_test.py
+++ RandomForest_test.py
@@ -3,13 +3,11 @@ import numpy as np
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 import math
-
+#last test
 df_train = pd.read_csv('data/cleaned_training_data.csv').iloc[0:100, :]
 df_test = pd.read_csv('data/cleaned_testing_data.csv')
 
 # np.random.seed(12345)
-# TEST_ML
-# Second Test on ML
 
 
 # random sample dataset
